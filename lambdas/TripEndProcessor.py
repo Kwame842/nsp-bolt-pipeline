f6d5464f577ba388c34ffefa06aff4f659b1c66c
Kwame842 <kwame.boateng@amalitechtraining.org>
2025-07-12 14:13:29 +0000

    Setup logging and DynamoDB for TripEndProcessor script

--- lambdas/TripEndProcessor.py
+++ lambdas/TripEndProcessor.py
@@ -6,3 +6,9 @@ import logging
 from datetime import datetime
 from boto3.dynamodb.conditions import Key
 import botocore.exceptions
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('TripData')
