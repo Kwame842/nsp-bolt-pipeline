3fbe0263f966fa311a01ad1761b0535570395717
Kwame842 <kwame.boateng@amalitechtraining.org>
2025-07-12 14:14:57 +0000

    Define required fields for TripEndProcessor script

--- lambdas/TripEndProcessor.py
+++ lambdas/TripEndProcessor.py
@@ -12,3 +12,18 @@ logger.setLevel(logging.INFO)
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('TripData')
+
+REQUIRED_FIELDS = {
+    "trip_id": str,
+    "dropoff_datetime": str,
+    "fare_amount": (float, int),
+}
+
+OPTIONAL_FIELDS = {
+    "tip_amount": (float, int),
+    "trip_distance": (float, int),
+    "passenger_count": int,
+    "rate_code": int,
+    "payment_type": int,
+    "trip_type": int
+}
